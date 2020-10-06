b38f215e0d53c52c59e4dac67806c2287225eff2
Igor Vukic <igor.vukic8877@gmail.com>
2020-10-06 22:36:18 +0200

    dodani korisnici

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -14,10 +14,6 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->insert([
-            'name' => Str::random(10),
-            'email' => Str::random(10).'gmail.com',
-            'password' => Hash::make('password')
-        ]);
+        factory(\App\User::class, 20)->create();
     }
 }

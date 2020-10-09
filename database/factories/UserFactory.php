a15e07e3850aa834d7aaf53a52a8fdb94d9396f4
Stanko Bebek <stanko.bebek82@gmail.com>
2020-10-09 18:08:13 +0200

    added city attr to users table

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,9 +18,12 @@ use Illuminate\Support\Str;
 */
 
 $factory->define(User::class, function (Faker $faker) {
+    $city_array = ['Sarajevo', 'Banja Luka','Tuzla','Zenica','Mostar','Bihać','Brčko','Trebinje','Travnik','Kiseljak','Doboj','Cazin','Široki Brijeg', 'Ljubuški', 'Čitluk', 'Čapljina'];
+
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
+        'mjesto' => $city_array[rand(0, 15)],
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
